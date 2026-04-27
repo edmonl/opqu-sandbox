537759d04d28f253b04c034e1b97da6467898f0f
Meng Luan <edmonluan@gmail.com>
2026-04-27 00:36:40 -0400

    organize go to cmd sbxctl

--- cmd/sbxctl/main.go
+++ cmd/sbxctl/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/edmonl/opqu-sandbox/cmd"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
+	if err := Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
