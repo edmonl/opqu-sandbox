9220c89a7253c475ce94215e570a74f428853b79
Meng Luan <edmonluan@gmail.com>
2026-05-09 15:26:35 -0400

    trivial fix in delete

--- cmd/sbx/delete.go
+++ cmd/sbx/delete.go
@@ -17,12 +17,11 @@ var deleteCmd = &cobra.Command{
 	Short: "Delete a sandbox",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		name := args[0]
-
 		if err := sandbox.Sudo(sbxDir); err != nil {
 			return err
 		}
 
+		name := args[0]
 		if err := sandbox.EnsureStopped(name); err != nil {
 			return err
 		}
