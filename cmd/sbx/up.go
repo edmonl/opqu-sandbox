549a3d5ce0799085a5589113ffc8b400e0b78c79
Meng Luan <edmonluan@gmail.com>
2026-05-24 20:01:49 -0400

    fix up command

--- cmd/sbx/up.go
+++ cmd/sbx/up.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	_ "embed"
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
@@ -39,65 +41,87 @@ var upCmd = &cobra.Command{
 		if running, err := sandbox.IsRunning(name); err != nil {
 			return err
 		} else if running {
-			fmt.Fprintf(os.Stderr, "Warning: sandbox %v is already running\n", name)
-		} else {
-			conf, err := config.LoadConf(sbxDir, name)
-			if err != nil {
-				return err
-			}
-
-			mounts, err := config.LoadMounts(sbxDir, name, conf.SandboxUser)
-			if err != nil {
-				return err
-			}
-
-			tmpl, err := template.New("nspawn").Parse(nspawnTemplate)
-			if err != nil {
-				return fmt.Errorf("failed to parse nspawn template: %w", err)
-			}
-
-			var sb strings.Builder
-			if err := tmpl.Execute(&sb, nspawnTemplateData{Conf: conf, Mounts: mounts}); err != nil {
-				return fmt.Errorf("failed to execute nspawn template: %w", err)
-			}
-
-			nspawnContent := sb.String()
-			nspawnPath := filepath.Join(conf.ImagePath, name+".nspawn")
-
-			if _, err := os.Stat(nspawnPath); err == nil {
-				prompt := fmt.Sprintf("File %v already exists. Press <Enter> directly to overwrite, or Ctrl+C to cancel: ", nspawnPath)
-				input, err := util.Confirm(prompt)
-				if err != nil {
-					return err
-				}
-				if input != "" {
-					return fmt.Errorf("user cancelled overwriting %v", nspawnPath)
-				}
-			}
-
-			removeNspawnFile := func() {
-				if err := os.RemoveAll(nspawnPath); err != nil {
-					fmt.Fprintf(os.Stderr, "Warning: failed to remove nspawn file %v: %v\n", nspawnPath, err)
-				}
-			}
-
-			if err := os.WriteFile(nspawnPath, []byte(nspawnContent), 0o644); err != nil {
-				removeNspawnFile()
-				return fmt.Errorf("failed to create nspawn file %v: %w", nspawnPath, err)
-			}
-
-			if err := sandbox.RunCmd("machinectl", "start", name); err != nil {
-				removeNspawnFile()
-				return fmt.Errorf("failed to start sandbox %v using machinectl: %w", name, err)
-			}
+			util.Warn("sandbox %v is already running", name)
+			return nil
 		}
 
-		if err := sandbox.RunCmd("machinectl", "enable", name); err != nil {
-			fmt.Fprintf(os.Stderr, "Warning: failed to enable sandbox %v: %v\n", name, err)
+		conf, err := config.LoadConf(sbxDir, name)
+		if err != nil {
+			return err
+		}
+
+		rootfsPath := filepath.Join(sbxDir, "rootfs", name)
+		imageSymlink := filepath.Join(conf.ImagesPath, name)
+		if ok, e := util.CheckSymlinkTarget(imageSymlink, rootfsPath); e != nil {
+			return fmt.Errorf("invalid sandbox image symlink: %w", e)
+		} else if !ok {
+			return fmt.Errorf("sandbox image symlink %v does not point to rootfs %v", imageSymlink, rootfsPath)
+		}
+
+		mounts, err := config.LoadMounts(sbxDir, name, conf.SandboxUser)
+		if err != nil {
+			return err
+		}
+
+		if e := os.MkdirAll(conf.NspawnFilesPath, 0o755); e != nil {
+			return fmt.Errorf("failed to create nspawn files directory %v: %w", conf.NspawnFilesPath, e)
+		}
+
+		nspawnPath, err := createNspawnFile(filepath.Join(sbxDir, "rootfs"), name, conf, mounts)
+		if err != nil {
+			return fmt.Errorf("failed to create nspawn file: %w", err)
+		}
+
+		if err := sandbox.CreateSymlink(nspawnPath, filepath.Join(conf.NspawnFilesPath, name+".nspawn")); err != nil {
+			return fmt.Errorf("failed to create nspawn file symlink: %w", err)
+		}
+
+		if err := util.RunCmd("machinectl", "start", name); err != nil {
+			return fmt.Errorf("failed to start sandbox %v using machinectl: %w", name, err)
+		}
+
+		if err := util.RunCmd("machinectl", "enable", name); err != nil {
+			util.Warn("failed to enable sandbox %v: %v", name, err)
 		}
 
 		return nil
 	},
+}
+
+func createNspawnFile(rootfsDir, name string, conf *config.Config, mounts []*config.Mount) (string, error) {
+	nspawnPath := filepath.Join(rootfsDir, name+".nspawn")
+
+	info, err := os.Stat(nspawnPath)
+	if err == nil {
+		if info.Mode().IsRegular() {
+			prompt := fmt.Sprintf("File %v already exists. Press <Enter> directly to overwrite, or Ctrl+C to cancel: ", nspawnPath)
+			if confirmed, e := util.Confirm(prompt); e != nil {
+				return "", e
+			} else if !confirmed {
+				return "", fmt.Errorf("user cancelled overwriting %v", nspawnPath)
+			}
+		} else {
+			return "", fmt.Errorf("%v exists but is not a regular file", nspawnPath)
+		}
+	} else if !errors.Is(err, fs.ErrNotExist) {
+		return "", fmt.Errorf("failed to access %v: %w", nspawnPath, err)
+	}
+
+	tmpl, err := template.New("nspawn").Parse(nspawnTemplate)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse nspawn template: %w", err)
+	}
+
+	var sb strings.Builder
+	if err := tmpl.Execute(&sb, nspawnTemplateData{Conf: conf, Mounts: mounts}); err != nil {
+		return "", fmt.Errorf("failed to execute nspawn template: %w", err)
+	}
+
+	if err := os.WriteFile(nspawnPath, []byte(sb.String()), 0o644); err != nil {
+		return "", fmt.Errorf("failed to write %v: %w", nspawnPath, err)
+	}
+
+	return nspawnPath, nil
 }
 
 func init() {
